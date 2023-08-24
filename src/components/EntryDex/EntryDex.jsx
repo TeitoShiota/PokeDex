7f641cfcbe29fa20c6e0af51dd4703b4604f19cb
Ahu Karahan Kristiansen <Ahu@laflairmedia.dk>
2023-08-24 17:14:09 +0200

    index, nav, sprites, entry

--- src/components/EntryDex/EntryDex.jsx
+++ src/components/EntryDex/EntryDex.jsx
@@ -190,7 +190,7 @@ export default function EntryDex() {
                     <ul>
                       {pokemon?.held_items.map((info) => (
                         <li>
-                          {info.item.name} <br /> {/* if none add text none *!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/}
+                          {info.item.name} <br /> 
                           {/* {info.version_details.name} */}
                         </li>
                         )) //stats er et array (af data [] på siden), så vi mapper den. ({} objekt)
